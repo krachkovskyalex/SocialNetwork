07564f191aa4844802e17be5a9f9e3b8fdca3252
Alexey Krachkovsky <krachking@ya.ru>
2022-11-18 15:10:58 +0300

    Initial commit

---
+++ app/src/main/java/com/krachkovsky/socialnetwork/ui/theme/Theme.kt
@@ -0,0 +1,24 @@
+package com.krachkovsky.socialnetwork.ui.theme
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.runtime.Composable
+
+private val DarkColorPalette = darkColors(
+    primary = VioletAccent,
+    background = DarkGray,
+    onBackground = TextWhite,
+    onPrimary = DarkGray,
+    surface = MediumGray,
+    onSurface = LightGray
+)
+
+@Composable
+fun SocialNetworkTheme(content: @Composable () -> Unit) {
+    MaterialTheme(
+        colors = DarkColorPalette,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
